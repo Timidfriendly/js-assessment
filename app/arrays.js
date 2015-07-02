31608e190cb8bcb93ec90dd9c30f7ae760dfa1a3
Timidfriendly <timid@point-online.org>
2015-07-02 21:56:48 +0200

    Arrays > indexOf Arrays > sum

--- app/arrays.js
+++ app/arrays.js
@@ -3,11 +3,25 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+      var i = 0;
+      for (len = arr.length; i < len; i++) {
+          if (arr[i] === item) {
+              return i;
+          }
+      }
+      return -1;
   },
 
   sum : function(arr) {
 
+          var sum = 0;
+      var i = 0;
+      for (len = arr.length; i < len; i++) {
+          sum = sum + arr[i];
+
+      }
+      console.log(sum);
+      return sum;
   },
 
   remove : function(arr, item) {
